79ed63377549c3268c00422efc53bc769a5afed2
Irek Nigmatullin <nigma143@mail.ru>
2021-03-04 21:29:41 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # actix-web-buffering
-Request/Response body buffering with support spooled to a temp file on disk
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod buffering;
+
+pub use crate::buffering::{
+    enable_request_buffering, enable_response_buffering, FileBufferingStreamWrapper,
+};
